373d09b26c0987aed37fb285a5d33fd8a0da1897
afnan <afnanabbas731@gmail.com>
2025-11-26 13:54:49 +0530

    Changes made by Asian guy Timezone

--- src/test/java/PageObjectModel/stepDefinition/stepDefinition.java
+++ src/test/java/PageObjectModel/stepDefinition/stepDefinition.java
@@ -26,6 +26,11 @@ public class stepDefinition extends BaseTest {
 	public void I_landed_on_Ecommerce_Page() throws IOException {
 		landingpage = launchApplication();
 	}
+	
+	@Given("I landed on Ecommerce Page")
+	public void I_landed_on_Ecommerce_Pages() throws IOException {
+		System.out.println("I landed on Ecommerce Page");
+	
 
 	@Given("^Logged in with username (.+) and password (.+)$")
 	public void logged_in_username_and_password(String username, String password) {
